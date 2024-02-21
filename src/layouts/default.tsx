686da8cc2d7b4a9133d1c04b9ddbfd552790077a
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-21 11:59:25 -0300

    add responsive layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/sidebar";
 
+import './default.css'
+
 export function Default() {
   return (
     <div className="layout">
